7be094234b6e214617786c5260a909b5dc924785
Julia Grigoreva <julia_panina@mail.ru>
2024-03-06 19:45:54 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,19 @@
-// const toggleMenu = document.querySelector('menu-toggle');
+const navBtn = document.querySelector('#mob-nav-btn');
+const nav = document.querySelector('.mobile-nav');
+const menuIcon = document.querySelector('.nav-icon');
 
-// if (toggleMenu){
-//     toggleMenu.addEventListener('click', function(){
+
+
+if (navBtn){
+      navBtn.addEventListener('click', function(){
         
-//         if(this.classList.contains('active')){
-//             this.classList.remove('active');
-//         }else{
-//             this.classList.add('active');
-//         }
-//     })
-// }
+        if(this.classList.contains('active')){
+            this.classList.remove('active');
+        }else{
+            this.classList.add('active');
+        }
+    })
+}
 
 // закрытие секции tagline
 const closeTaglineBtn = document.querySelector('.tagline__close');
